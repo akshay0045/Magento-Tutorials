5af2270cff2d30fce8816231160b7ea758bcfb95
akshaytechsevin <akshay.shah451981@gmail.com>
2021-06-11 20:41:49 +0530

    insert update delete mass delete inline update and import data using svg file

---
+++ app/code/Ambab/Exchange/view/frontend/requirejs-config.js
@@ -0,0 +1,7 @@
+var config = {
+    map: {
+        '*': {
+            ambab_exchangejs:  'Ambab_Exchange/js/custom_exchange'
+        }
+    }
+};
--- app/etc/config.php
+++ app/etc/config.php
@@ -330,6 +330,7 @@ return [
         'Amazon_Core' => 1,
         'Amazon_Login' => 1,
         'Amazon_Payment' => 1,
+        'Ambab_Exchange' => 1,
         'Dotdigitalgroup_Email' => 1,
         'Dotdigitalgroup_Chat' => 1,
         'Dotdigitalgroup_Sms' => 1,
