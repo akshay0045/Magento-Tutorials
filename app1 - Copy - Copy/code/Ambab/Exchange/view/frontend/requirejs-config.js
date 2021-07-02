3404437947032146a04765bd05cacd51d6236ade
akshaytechsevin <akshay.shah451981@gmail.com>
2021-07-02 11:01:29 +0530

    changes for custom module

--- app1 - Copy - Copy/code/Ambab/Exchange/view/frontend/requirejs-config.js
+++
@@ -1,7 +0,0 @@
-var config = {
-    map: {
-        '*': {
-            ambab_exchangejs:  'Ambab_Exchange/js/custom_exchange'
-        }
-    }
-};
--- app1 - Copy - Copy/code/Rsgitech/News/Block/Adminhtml/Allnews/Grid.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace Rsgitech\News\Block\Adminhtml\Allnews;
-
-class Grid extends \Magento\Backend\Block\Widget\Grid
-{
- 
-}
